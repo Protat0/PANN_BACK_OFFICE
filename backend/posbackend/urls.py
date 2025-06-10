5cdbd1554a87930e1f9766426585817d65a39c3d
Sceyo <73180039+Sceyo@users.noreply.github.com>
2025-06-10 23:39:43 +0800

    update 1

--- backend/posbackend/urls.py
+++ backend/posbackend/urls.py
@@ -20,7 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('admin/', admin.site.urls),
     path('notifications/', include('notifications.urls')),
 ]
     
